06616ca1795df00102f90eda9d23dbe0a81ed586
Tjalling Haije <tjalling_haije@outlook.com>
2026-02-27 22:26:24 +0100

    fixed scenario runner shutdown error

--- scripts/map_publisher.py
+++ scripts/map_publisher.py
@@ -83,7 +83,8 @@ def main() -> None:
         pass
     finally:
         node.destroy_node()
-        rclpy.shutdown()
+        if rclpy.ok():
+            rclpy.shutdown()
 
 
 if __name__ == "__main__":
